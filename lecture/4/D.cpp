3ca3aed5857d47a91552910c87a1077d05879ef9
Thomas Wienecke <wienecke.t@gmail.com>
2015-12-18 13:07:47 +0100

    Reduce lines of code in lecture/4/D.

--- lecture/4/D.cpp
+++ lecture/4/D.cpp
@@ -10,18 +10,16 @@ int J;
 bool oper[MAXN], changeable[MAXN], value[MAXN];
 // oper is 1 = AND, 0 = OR
 
+bool evalOperation(bool operation, bool left, bool right) {
+    return (  (operation && (left && right))
+           || (!operation && (left || right))
+           );
+}
+
 bool evaluate(int vertex){
-   if (vertex > J){
-      return value[vertex];
-   }
-   else{
-      if (oper[vertex]){
-         value[vertex] = evaluate(vertex*2) & evaluate(vertex*2 + 1);
-      }
-      else{
-         value[vertex] = evaluate(vertex*2) | evaluate(vertex*2 + 1);
-      }
-   }
+   if (vertex > J) return value[vertex];
+
+   value[vertex] = evalOperation(oper[vertex], evaluate(vertex*2), evaluate(vertex*2 + 1));
    return value[vertex];
 }
 
@@ -34,67 +32,24 @@ int dp(int vertex, bool des_val){
       }
       else return inf;
    }
-   else{
-      bool left = value[2*vertex];
-      bool right = value[2*vertex + 1];
-      // if it is alright
-      if (oper[vertex] && ((left & right) == des_val)) return 0;
-      if (!oper[vertex] && ((left | right) == des_val)) return 0;
 
-      if (!changeable[vertex]){
-         // try to change values
-         if (des_val){
-            if (oper[vertex]){
-               int sum = 0;
-               if (!left) sum += dp(2*vertex, 1);
-               if (!right) sum += dp(2*vertex+1, 1);
-               return sum;
-            }
-            else{
-               // case OR
-               return min(dp(2*vertex,1), dp(2*vertex+1, 1));
-            }
-         }
-         else{
-            // we want to have 0
-            if (oper[vertex]){
-               return min(dp(2*vertex,0), dp(2*vertex+1, 0));
-            }
-            else{
-               // case OR
-               int sum = 0;
-               if (left) sum += dp(2*vertex, 0);
-               if (right) sum += dp(2*vertex+1, 0);
-               return sum;
-            }
-         }
-      }
-      // if changeable
-      // want 0, have AND, and not alright --> currently we have (1,1)
-      if (!des_val && oper[vertex]) return min(dp(2*vertex, 0), dp(2*vertex+1, 0));
+   bool left = value[2*vertex];
+   bool right = value[2*vertex + 1];
+   bool operation = oper[vertex];
+   // if it is alright
+   if (evalOperation(operation, left, right) == des_val) return 0;
 
-      // want 1, have OR, and not alright --> currently we have (0,0)
-      if (des_val && !oper[vertex]) return min(dp(2*vertex, 1), dp(2*vertex+1, 1));
+   if (!changeable[vertex]){
+      // try to change child values
 
-      // want 0, have OR, and not alright --> currently we have (1,1), (0,1) or (1,0)
-      // --> change to AND and look
-      if (!des_val && !oper[vertex]){
-         if (0 == (left & right)) return 1;
-         else{
-            // case (1,1)
-            return min(1+dp(2*vertex, 0), 1+dp(2*vertex+1, 0));
-         }
-      }
-      // want 1, have AND, and not alright --> currently we have (1,1), (0,1) or (1,0)
-      // --> change to OR and look
-      if (des_val && oper[vertex]){
-         if (1 == (left | right)) return 1;
-         else{
-            // case (1,1)
-            return min(1+dp(2*vertex, 1), 1+dp(2*vertex+1, 1));
-         }
-      }
+      if ((des_val && operation) || (!des_val && !operation))
+          return dp(2*vertex, des_val) + dp(2*vertex+1, des_val);
    }
+   // (des_val != operation)
+   if ((des_val && !operation) || (!des_val && operation))
+       return min(dp(2*vertex, des_val), dp(2*vertex+1, des_val));
+
+   return min(1+dp(2*vertex, des_val), 1+dp(2*vertex+1, des_val));
 }
 
 int main(void){
